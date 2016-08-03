fbdf2c507cc23402928933a703bcf91569448031
qianteng <936129056@qq.com>
2016-08-04 00:29:15 +0800

    强转
    强转方式

---
+++ javaBase/运算符/强转.java
@@ -0,0 +1,15 @@
+package javaBase.运算符;
+
+public class 强转 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/**
+ * 大类型转小类型
+ * 语法 反向转换 （需要转化成的类型）变量
+ * short b=200;
+ * int a=(int)b;
+ */
+	}
+
+}
